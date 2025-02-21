72dd46596d35047e7cc2cf4c7a193646f21265dd
Yersultan <129984720+yyersultan29@users.noreply.github.com>
2025-02-21 15:53:04 +0500

    done

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,8 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+
+        <h5>Chnages with pushka</h5>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
